1c028cbd80622db074abdeffbd2ce528615c1791
Vikas Khan <vikas.aslam.khan@gmail.com>
2018-04-17 07:39:41 +0000

    Ran RenameImages Program

--- RenameImages.py
+++ RenameImages.py
@@ -1,20 +1,21 @@
 import cv2
 
 counter = 0
+image = None
 
-for i in range(1:4000)
-    if 0 < i < 10:
-        image = cv2.imread('neg/neg-000' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
+for i in range(2000):
+    if 0 <= i < 10:
+        image = cv2.imread('NegImages/neg-000' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
     elif 10 <= i < 100:
-        image = cv2.imread('neg/neg-00' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
+        image = cv2.imread('NegImages/neg-00' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
     elif 100 <= i < 1000:
-        image = cv2.imread('neg/neg-0' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
+        image = cv2.imread('NegImages/neg-0' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
     elif 1000 <= i < 10000:
-        image = cv2.imread('neg/neg-' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread('NegImages/neg-' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
     if not image is None:
         new_image = cv2.resize(image, (100, 100))
-        cv2.imwrite("neg/neg"+str(counter)+".jpg",new_image)
-        line = "neg/neg"+str(counter)+".jpg\n"
+        cv2.imwrite("NegImages/neg"+str(counter)+".jpg",new_image)
+        line = "NegImages/neg"+str(counter)+".jpg\n"
         with open('bg.txt', 'a') as f:
             f.write(line)
         counter+=1
