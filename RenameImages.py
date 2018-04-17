fbb849ae73ab84e53c723c95a9e3fda6b823a5a4
Vikas Khan <vikas.aslam.khan@gmail.com>
2018-04-17 03:12:40 -0400

    Made some edits

--- RenameImages.py
+++ RenameImages.py
@@ -1,19 +1,20 @@
-import sys
-sys.path.insert(0, '~/opencv_workspace/')
-import opencv as cv2
+import cv2
 
 counter = 0
 
 for i in range(1:4000)
     if 0 < i < 10:
-        image = cv2.imread('neg-000' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread('neg/neg-000' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
     elif 10 <= i < 100:
-        image = cv2.imread('neg-00' + str(i))   
+        image = cv2.imread('neg/neg-00' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
     elif 100 <= i < 1000:
-        image = cv2.imread('neg-0' + str(i))   
+        image = cv2.imread('neg/neg-0' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)   
     elif 1000 <= i < 10000:
-        image = cv2.imread('neg-' + str(i))
+        image = cv2.imread('neg/neg-' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE)
     if not image is None:
         new_image = cv2.resize(image, (100, 100))
-        cv2.imwrite("neg"+str(counter)+".jpg",new_image)
+        cv2.imwrite("neg/neg"+str(counter)+".jpg",new_image)
+        line = "neg/neg"+str(counter)+".jpg\n"
+        with open('bg.txt', 'a') as f:
+            f.write(line)
         counter+=1
