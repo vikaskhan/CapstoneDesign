8c5e4157b25e1d28983ca6ffe924b175ee3c029c
Vikas Khan <vikas.aslam.khan@gmail.com>
2018-04-19 22:28:49 +0000

    Added image for poster

--- data/TestStop.py
+++ data/TestStop.py
@@ -10,19 +10,13 @@ from picamera import PiCamera
 import time
 import cv2
 
-while 1:
-    img = cv2.imread('testimage.jpg')
-    print('hello')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    stop = stop_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in stop:
-        print('nigga')
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img,'Stop Sign',(x-w,y-h), font, 0.5, (11,255,255), 2, cv2.LINE_AA)
-        cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+img = cv2.imread('testimage.jpg')
+print('hello')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+stop = stop_cascade.detectMultiScale(gray, 5, 5)
+for (x,y,w,h) in stop:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+cv2.imwrite('outputimage.jpg', img)
+k = cv2.waitKey(30) & 0xff
+
